4e701a61a4f99c0101e1738f6137966cdc9f6d01
Feliciano Long <noahzark@vip.qq.com>
2015-02-02 20:48:40 -0800

    Optimized code structure

--- XXXCocosLuaGame/src/Scene/GameScene.lua
+++ XXXCocosLuaGame/src/Scene/GameScene.lua
@@ -1,7 +1,6 @@
 --------------------------------------------------------------------------------
 -- GameBoardPanel.lua - 游戏场景
 -- @author fangzhou.long
--- TODO 将本场景包装成类
 --------------------------------------------------------------------------------
 
 require "core.BaseScene"
@@ -20,17 +19,18 @@ end
 
 -- create game scene
 function GameScene:onInit()
-    local GameBackgroundLayer = require("panel.GameBackgroundLayer")
-    self:addChild(GameBackgroundLayer.create())
-
     AudioEngine.stopMusic(true)
-
-    local bgMusicPath = cc.FileUtils:getInstance():fullPathForFilename("sound/bgm_game.wav")
-    AudioEngine.playMusic(bgMusicPath, true)
-
+    
+    local GameBackgroundLayer = require("panel.GameBackgroundLayer")
+    self.backLayer = GameBackgroundLayer.create()
+    self:addChild(self.backLayer)
+    
     local GameBoardPanel = require("panel.GameBoardPanel")
     self.gameBaord = GameBoardPanel.create()
     self:addChild(self.gameBaord)
+
+    local bgMusicPath = cc.FileUtils:getInstance():fullPathForFilename("sound/bgm_game.wav")
+    AudioEngine.playMusic(bgMusicPath, true)
 end
 
 return GameScene
