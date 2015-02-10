e1ff57ab41bad62e9741289211dae598dae9703e
Feliciano Long <noahzark@vip.qq.com>
2015-02-10 01:01:36 -0800

    Add SceneManager

---
+++ XXXCocosLuaGame/src/manager/SceneManager.lua
@@ -0,0 +1,46 @@
+--------------------------------------------------------------------------------
+-- SceneManager.lua - 游戏场景管理器
+-- @author fangzhou.long
+--------------------------------------------------------------------------------
+
+--------------------------------
+--  Scene manager for the game
+-- @module manager.SceneManager
+SceneManager = {}
+
+--------------------------------
+--  The method to change current scene to a new scene and deliver a parameter table.
+-- I really doubt whether we will use so I just left it alone --by fangzhou.long
+-- @function [parent=#SceneManager] replaceScene
+-- @param #string scene Target scene
+-- @param #table params Parameters passed to create target scene
+function SceneManager.replaceScene(scene, params)
+    --TODO Is this function really needed?
+    cclog("Warning! Uncompleted function called:" .. "SceneManager.replaceScene")
+end
+
+--------------------------------
+--  The method to change current scene to a new scene with its name and deliver a parameter table
+-- @function [parent=#SceneManager] replaceSceneWithName
+-- @param #string sceneName Target scene name
+-- @param #table params Parameters passed to create target scene
+function SceneManager.replaceSceneWithName(sceneName, params)
+    local ccRunning = cc.Director:getInstance():getRunningScene()
+    if ccRunning and ccRunning.onExit then
+        ccRunning:onExit()
+        -- ccRunning:dispose()
+    end
+    
+    local sceneClass = require("scene." .. sceneName)
+    local targetScene = sceneClass.create(params)
+    
+    if ccRunning then
+        cc.Director:getInstance():replaceScene(targetScene)
+    else
+        cc.Director:getInstance():runWithScene(targetScene)
+    end
+    
+    if targetScene.onEnter then
+        targetScene:onEnter()
+    end
+end
