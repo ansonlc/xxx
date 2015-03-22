ea4f94b68afac501cf7cfd5b81c19f144a2c9732
Rgnrtn <zhongchaomin@gmail.com>
2015-03-22 12:12:12 -0700

    added user_level_status, user_status

---
+++ XXXCocosLuaGame/src/config/user_level_status.lua
@@ -0,0 +1,8 @@
+local UserLevelStatusTable = {
+    [1001] = {userID =1001, levelNum=1001,score = 10000},
+    [1002] = {userID =1002, levelNum=1001,score = 11000},
+    [1003] = {userID =1003, levelNum=1001,score = 12000},
+    [1004] = {userID =1004, levelNum=1001,score = 13000},
+    [1005] = {userID =1005, levelNum=1001,score = 14000}
+}
+return UserLevelStatusTable
