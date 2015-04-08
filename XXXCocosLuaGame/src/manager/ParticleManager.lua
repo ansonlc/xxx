4613da48a8d15e7e0998a280eb38d42d9099bc0f
Chao Lin <linchaoc@gmail.com>
2015-04-08 11:12:24 -0700

    modify particle display function

--- XXXCocosLuaGame/src/manager/ParticleManager.lua
+++ XXXCocosLuaGame/src/manager/ParticleManager.lua
@@ -12,27 +12,39 @@ local playerXY = cc.p(1500,500)
 --  @param #float duration the particle effect duration
 --  @param #string plist the filename of the particle
 --  @param #int actionType 0 means use straight line, 1 means use arc
-function ParticleManager.particleDisplay(from,to,parentNode,duration,plist)
-    local particle = cc.ParticleSystemQuad:create(plist)
-    particle:setPosition(from)
-    particle:setDuration(duration+0.1)
-    parentNode:addChild(particle)
+--function ParticleManager.particleDisplay(from,to,parentNode,duration,plist)
+--    local particle = cc.ParticleSystemQuad:create(plist)
+--    particle:setPosition(from)
+--    particle:setDuration(duration+0.1)
+--    parentNode:addChild(particle)
+--    local moveToAction = cc.MoveTo:create(duration,to)
+--    particle:runAction(moveToAction)
+--end
+
+function ParticleManager.particleDisplay(from,to,parentNode,duration,particleID)
+   
+    local particle = MetaManager.getParticle(particleID)
+    local particleNode = cc.ParticleSystemQuad:create(particle.path)
+    particleNode:setPosition(from)
+    particleNode:setDuration(duration+0.1)
+    parentNode:addChild(particleNode)
 
     local moveToAction = cc.MoveTo:create(duration,to)
-    particle:runAction(moveToAction)
+    particleNode:runAction(moveToAction)
 end
 
-function ParticleManager.particleDisplay(from,to,parentNode,duration,plist,actionType)
-    local particle = cc.ParticleSystemQuad:create(plist)
-    particle:setPosition(from)
-    particle:setDuration(duration+0.1)
-    parentNode:addChild(particle)
+--[[function ParticleManager.particleDisplay(from,to,parentNode,duration,plist,actionType)
+--    local particle = cc.ParticleSystemQuad:create(plist)
+--    particle:setPosition(from)
+--    particle:setDuration(duration+0.1)
+--    parentNode:addChild(particle)
+--    local moveAction = cc.MoveTo:create(duration,to)
+--    if 1==actionType then
+--    end    
+--    particle:runAction(moveAction)
+--end--]]
 
-    local moveAction = cc.MoveTo:create(duration,to)
-    if 1==actionType then
-    end    
-    particle:runAction(moveAction)
-end
+
 
 function ParticleManager.monsterUseSkillParticle(parentNode,skill)
     local particle  = MetaManager.getParticle(skill.particleID)
