7d461a9cba58d7bbfde0948ff1bfdc7883c70f2f
Feliciano Long <noahzark@vip.qq.com>
2015-04-14 14:10:59 -0700

    Add comments to core.GameButton

--- XXXCocosLuaGame/src/core/GameButton.lua
+++ XXXCocosLuaGame/src/core/GameButton.lua
@@ -13,6 +13,12 @@ GameButton = class("GameButton", function() return ccui.Button:create() end)
 
 function GameButton:ctor() end
 
+--------------------------------
+--  Initialize method of the scene, will be execute when called "created"
+--  @function [parent=#GameButton] init
+--  @param self
+--  @param #string text Text key
+--  @param #bool withBg Load background texture or not
 function GameButton:init(text, withBg)
     local btnConfig = MetaManager.getBtnUI(text)
     --Load background texture
@@ -51,12 +57,26 @@ function GameButton:init(text, withBg)
     end
 end
 
+--------------------------------
+--  Set title text of game button
+--  @function [parent=#GameButton] setTitle
+--  @param self
+--  @param #string Title text
+--  @param #bool fontName Title font name
+--  @param #int size Title font size
 function GameButton:setTitle(text, fontName, size)
     self:setTitleText(text)
     self:setTitleFontName(fontName)
     self:setTitleFontSize(size)
 end
 
+--------------------------------
+--  Create method of the game button
+--  @function [parent=#GameButton] create
+--  @param #string text Text key
+--  @param #bool withBg Load background texture or not (Optional, default = nil)
+--  @param #float scale Scale of the button (Optional, default = 1x)
+--  @return #core.GameButton Created game button
 function GameButton.create(text, withBg, scale)
     local button = GameButton.new()
     --Set touch
@@ -70,15 +90,25 @@ function GameButton.create(text, withBg, scale)
     end
     
     --Add cover layer
-    local coverLayer = cc.LayerColor:create(cc.c4b(0, 0, 0,150), 150, 60)
-    coverLayer:setVisible(false)
-    button.coverLayer = coverLayer
-    button:addChild(coverLayer)
+    local btnConfig = MetaManager.getBtnUI(text)
+    if withBg and btnConfig and btnConfig.disabled and btnConfig.disabled == "normal" then
+        local coverLayer = cc.LayerColor:create(cc.c4b(0, 0, 0,150), 150, 60)
+        coverLayer:setVisible(false)
+        button.coverLayer = coverLayer
+        button:addChild(coverLayer)
+    end
     
     return button;
 end
 
+--------------------------------
+--  Set enabled function of game button
+--  @function [parent=#GameButton] setEnabled
+--  @param self
+--  @param #bool enabled Enable flag
 function GameButton:setEnabled(enabled)
     ccui.Widget.setEnabled(self, enabled) --ccui.Button doesnt have a setEnabled method
-    self.coverLayer:setVisible(not enabled)
+    if self.coverLayer then
+        self.coverLayer:setVisible(not enabled)
+    end
 end
