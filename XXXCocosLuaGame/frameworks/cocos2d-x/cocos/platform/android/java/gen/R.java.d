e73e4b168294a4ccaa6a16a293799c5570258631
Feliciano Long <noahzark@vip.qq.com>
2015-04-15 11:23:18 -0700

    Push built compile environment, dont modify this branch!

---
+++ XXXCocosLuaGame/frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\Users\fangzhou\Documents\Works\xxx\XXXCocosLuaGame\frameworks\cocos2d-x\cocos\platform\android\java\bin\AndroidManifest.xml \
---
+++ XXXCocosLuaGame/frameworks/runtime-src/proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+C:\Users\fangzhou\Documents\Works\xxx\XXXCocosLuaGame\frameworks\runtime-src\proj.android\gen\org\cocos2dx\XXXCocosLuaGame\R.java \
+ : C:\Users\fangzhou\Documents\Works\xxx\XXXCocosLuaGame\frameworks\runtime-src\proj.android\res\drawable-hdpi\icon.png \
+C:\Users\fangzhou\Documents\Works\xxx\XXXCocosLuaGame\frameworks\runtime-src\proj.android\res\drawable-ldpi\icon.png \
+C:\Users\fangzhou\Documents\Works\xxx\XXXCocosLuaGame\frameworks\runtime-src\proj.android\res\drawable-mdpi\icon.png \
+C:\Users\fangzhou\Documents\Works\xxx\XXXCocosLuaGame\frameworks\runtime-src\proj.android\res\values\strings.xml \
+C:\Users\fangzhou\Documents\Works\xxx\XXXCocosLuaGame\frameworks\runtime-src\proj.android\bin\AndroidManifest.xml \
