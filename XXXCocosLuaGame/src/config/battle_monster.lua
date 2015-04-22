3058928a523add59c9c3d609e9c19063b85f2e60
cgy4ever <root@cgy.ac>
2015-04-22 10:46:47 -0700

    update 'pikachu' to 'Pikachu'

--- XXXCocosLuaGame/src/config/battle_monster.lua
+++ XXXCocosLuaGame/src/config/battle_monster.lua
@@ -13,9 +13,23 @@ local MonsterTable = {
     	monsterHP=2000,
     	elementTable={physical=0.1,air=0.1,earth=0.1,water=0.1,fire=0.1},
     	skillTable={SkillID1=-10021,SkillID2=-10021,SkillID3=-10021},
-    	picture = "pikachu"
+        picture = "Pikachu"
     	},
-    [1003] = {monsterID =1003,monsterName='Water Fairy',monsterHP=600,elementTable={physical=0.5,air=0.5,earth=1.0,water=-1.0,fire=0.0},skillTable={SkillID1=1001,SkillID2=1006,SkillID3=1007}},
-    [1004] = {monsterID =1004,monsterName='Fire Fairy',monsterHP=400,elementTable={physical=0.5,air=0,earth=0.25,water=1.0,fire=-1.0},skillTable={SkillID1=1001,SkillID2=1008,SkillID3=1009}},
+    [1003] = {
+        monsterID =1003,
+        monsterName='pipikachu~',
+        monsterHP=3000,
+        elementTable={physical=0.1,air=0.1,earth=0.1,water=0.1,fire=0.1},
+        skillTable={SkillID1=-10021,SkillID2=-10021,SkillID3=-10021},
+        picture = "Pikachuaa"
+    },
+    [1004] = {
+        monsterID =1004,
+        monsterName='pipikachu~',
+        monsterHP=4000,
+        elementTable={physical=0.1,air=0.1,earth=0.1,water=0.1,fire=0.1},
+        skillTable={SkillID1=-10021,SkillID2=-10021,SkillID3=-10021},
+        picture = "Pikachuaa"
+    },
 }
 return MonsterTable
