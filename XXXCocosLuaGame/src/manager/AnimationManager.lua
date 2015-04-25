170f27de28fc41ac2454da0a6fb90aee4273a69d
Feliciano Long <noahzark@vip.qq.com>
2015-04-25 01:03:42 -0700

    Load new animation and modified default delay time

--- XXXCocosLuaGame/src/manager/AnimationManager.lua
+++ XXXCocosLuaGame/src/manager/AnimationManager.lua
@@ -10,10 +10,22 @@ local Animations = require("config.animation")
 function AnimationManager.loadAnime(name)
     local cacheInst = cc.SpriteFrameCache:getInstance()
     cacheInst:addSpriteFrames(Animations[name].src)
+    local spriteFrame = cacheInst:getSpriteFrame(name .. "_01.png")
+    
+    if spriteFrame then
+        cclog("Info: Animation - " .. name .. " loaded.")
+    else
+        cclog("Error: Animation - " .. name .. " load failed, please check the file.")
+    end
 end
 
 function AnimationManager.init()
     AnimationManager.loadAnime("Attack1")
+    AnimationManager.loadAnime("Attack5")
+    AnimationManager.loadAnime("Heal1")
+    AnimationManager.loadAnime("Heal2")
+    AnimationManager.loadAnime("Heal3")
+    AnimationManager.loadAnime("Heal4")
 end
 
 function AnimationManager.create(name, delay, loops)
@@ -43,7 +55,7 @@ function AnimationManager.create(name, delay, loops)
     
     --Assemble animation
     local animation = cc.Animation:createWithSpriteFrames(frames,
-        delay and delay or 0.1,
+        delay and delay or 0.05,
         loops and loops or 1)
     animation:setRestoreOriginalFrame(false)
     local animate = cc.Animate:create(animation)
