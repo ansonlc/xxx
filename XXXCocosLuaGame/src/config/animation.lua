1d61631beccf9c30633c7fdfbad5a0b5c3a48205
Feliciano Long <noahzark@vip.qq.com>
2015-04-25 01:03:11 -0700

    Add animation to config

--- XXXCocosLuaGame/src/config/animation.lua
+++ XXXCocosLuaGame/src/config/animation.lua
@@ -1,4 +1,9 @@
 local animation = {
-    ["Attack1"] = {src="animations/Attack1.plist", frames = 3}
+    ["Attack1"] = {src="animations/Attack1.plist", frames = 3},
+    ["Attack5"] = {src="animations/Attack5.plist", frames = 7},
+    ["Heal1"] = {src="animations/Heal1.plist", frames = 30},
+    ["Heal2"] = {src="animations/Heal2.plist", frames = 18},
+    ["Heal3"] = {src="animations/Heal3.plist", frames = 30},
+    ["Heal4"] = {src="animations/Heal4.plist", frames = 22},
 }
 return animation
