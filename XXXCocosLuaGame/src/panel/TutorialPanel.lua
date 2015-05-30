754aaf233c06f2c56ab6491ba60806779a6be395
Feliciano Long <noahzark@vip.qq.com>
2015-05-30 21:13:21 +0800

    Update Tutorial Panel: Finish major functions

--- XXXCocosLuaGame/src/panel/TutorialPanel.lua
+++ XXXCocosLuaGame/src/panel/TutorialPanel.lua
@@ -7,9 +7,64 @@ local GameOptionPanel = class("GameOptionPanel", function() return cc.Layer:crea
 
 local visibleSize = cc.Director:getInstance():getVisibleSize()
 
-function GameOptionPanel:create(parent)
+function GameOptionPanel.create(scene, tutorialBtn)
     local layer = GameOptionPanel.new()
-    layer:initLayer()
-    parent.addChild(layer)
+    layer:initLayer(scene, tutorialBtn)
     return layer
 end
+
+function GameOptionPanel:initLayer(scene, tutorialBtn)
+if scene.sceneName then
+    local sprite = cc.Sprite:create("imgs/tutorial/" .. scene.sceneName .. ".png")
+if sprite then
+    sprite:setPosition(visibleSize.width/2, visibleSize.height/2)
+    self:addChild(sprite)
+    
+    local dispatcher = cc.Director:getInstance():getEventDispatcher()
+    local listener = cc.EventListenerTouchOneByOne:create()
+    
+    local function onTouch(touch, event)
+        if event:getEventCode() == ccui.TouchEventType.ended then
+            sprite:runAction(cc.FadeOut:create(0.5))
+            scene.touchEnabled = true
+            listener:setEnabled(false)
+            if scene.setGameTouch then
+                scene:setGameTouch(true)
+            end
+        end
+        
+        return true
+    end
+    
+    listener:registerScriptHandler(onTouch, cc.Handler.EVENT_TOUCH_BEGAN)
+    listener:registerScriptHandler(onTouch, cc.Handler.EVENT_TOUCH_MOVED)
+    listener:registerScriptHandler(onTouch, cc.Handler.EVENT_TOUCH_ENDED)
+    dispatcher:addEventListenerWithSceneGraphPriority(listener, scene)
+    listener:setSwallowTouches(true)
+    
+    local function onBtnPress()
+        print("touch")
+        if not scene.touchEnabled then return true end
+        
+        if scene.setGameTouch then
+            scene:setGameTouch(false)
+        end
+                
+        scene.touchEnabled = false
+        sprite:runAction(cc.FadeIn:create(0.5))
+        listener:setEnabled(true)
+
+        return true
+    end
+    
+    tutorialBtn:addTouchEventListener(onBtnPress)
+    
+    listener:setEnabled(false)
+    sprite:setOpacity(0)
+else
+    cclog("ERROR: This scene dont have a tutorial")
+end
+end
+end
+
+return GameOptionPanel
