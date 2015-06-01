55dd855bb1805b04a3e0700188f526d933e55b93
Feliciano Long <noahzark@vip.qq.com>
2015-06-01 13:18:01 +0800

    Replace boss picture in battle_monster config

--- XXXCocosLuaGame/src/config/battle_monster.lua
+++ XXXCocosLuaGame/src/config/battle_monster.lua
@@ -5,7 +5,7 @@ local MonsterTable = {
     	monsterHP=200,
     	elementTable={physical=1.5,air=0.3,earth=0.3,water=0.3,fire=0.3},
     	skillTable={SkillID1=-10011,SkillID2=-10011,SkillID3=-10011},
-        picture = "monster_air_bird"
+        picture = "2"
     	},
     [1002] = {
     	monsterID =1002,
@@ -13,7 +13,7 @@ local MonsterTable = {
     	monsterHP=400,
     	elementTable={physical=1.0,air=1.0,earth=1.0,water=1.0,fire=1.0},
     	skillTable={SkillID1=-10021,SkillID2=-10021,SkillID3=-10021},
-        picture = "monster_earth_rock"
+        picture = "2-1"
     	},
     [1003] = {
         monsterID =1003,
@@ -21,7 +21,7 @@ local MonsterTable = {
         monsterHP=600,
         elementTable={physical=3.0,air=1.0,earth=1.0,water=1.0,fire=1.0},
         skillTable={SkillID1=-10021,SkillID2=-10021,SkillID3=-10021},
-        picture = "RemiliaSWR"
+        picture = "3"
     },
     [1004] = {
         monsterID = 1004,
@@ -29,7 +29,7 @@ local MonsterTable = {
         monsterHP = 2000,
         elementTable={physical=4.0,air=0.1,earth=0.1,water=0.1,fire=0.1},
         skillTable={SkillID1=-10021,SkillID2=-10021,SkillID3=-10021},
-        picture = "monster_fire_skull"
+        picture = "3-1"
     },
     [1005] = {
         monsterID = 1005,
@@ -37,7 +37,7 @@ local MonsterTable = {
         monsterHP= 1500,
         elementTable={physical=1.0,air=4.0,earth=4.0,water=4.0,fire=4.0},
         skillTable={SkillID1=-10021,SkillID2=-10021,SkillID3=-10021},
-        picture = "monster_water_element"
+        picture = "5"
     },
     [1006] = {
         monsterID = 1006,
@@ -45,7 +45,7 @@ local MonsterTable = {
         monsterHP= 2000,
         elementTable={physical=7.0,air=1.0,earth=1.0,water=1.0,fire=1.0},
         skillTable={SkillID1=-10021,SkillID2=-10021,SkillID3=-10021},
-        picture = "flandre"
+        picture = "5-1"
     },
 
 
