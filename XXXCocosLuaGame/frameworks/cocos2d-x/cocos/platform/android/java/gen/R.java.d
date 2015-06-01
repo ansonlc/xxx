a9838bd42f107452aeba2a58d7456c9854d3c2d7
Feliciano Long <longf_removehere_@usc.edu>
2015-06-01 13:28:31 +0800

    Push some mid files for packing

--- XXXCocosLuaGame/frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
+++ XXXCocosLuaGame/frameworks/cocos2d-x/cocos/platform/android/java/gen/R.java.d
@@ -1,1 +1,1 @@
- : C:\Users\fangzhou\Documents\Works\xxx\XXXCocosLuaGame\frameworks\cocos2d-x\cocos\platform\android\java\bin\AndroidManifest.xml \
+ : C:\Users\Feliciano\Documents\Cocos\xxx\XXXCocosLuaGame\frameworks\cocos2d-x\cocos\platform\android\java\bin\AndroidManifest.xml \
